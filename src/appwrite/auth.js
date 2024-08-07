dadb494d059691e9302cab1a08ffe3e0e7418a25
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-08-07 11:27:35 +0530

    Initial commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.Client.setEndpoint(conf.apppwriteUrl).setProject(
+      conf.appwriteProjectId
+    );
+
+    this.account = new Account(client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        password,
+        email,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrencUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser() :: ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout() :: ", error);
+    }
+  }
+}
